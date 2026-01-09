fe3329e245af945beaee7c5bd62ad6ed19274a18
Saanvi Gupta <saanvigupta372@gmail.com>
2026-01-09 21:04:23 +0530

    Initial commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3001
 
 app.use(express.static(path.join(__dirname, "public")))
+app.use("/images", express.static(path.join(__dirname, "images")))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "templates/index.html"))
